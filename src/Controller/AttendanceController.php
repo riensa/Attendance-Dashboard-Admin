1a8226cbb4c73028d89c174962c48a67a5d37b58
Endah Sari <riri.mayries@gmail.com>
2022-07-21 23:29:12 +0700

    codebase

---
+++ src/Controller/AttendanceController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AttendanceController extends AbstractController
+{
+    /**
+     * @Route("/attendance", name="app_attendance")
+     */
+    public function index(): Response
+    {
+
+        $data = [
+            'Apple' => '$1.16 trillion USD',
+            'Samsung' => '$298.68 billion USD',
+            'Microsoft' => '$1.10 trillion USD',
+            'Alphabet' => '$878.48 billion USD',
+            'Intel Corporation' => '$245.82 billion USD',
+            'IBM' => '$120.03 billion USD',
+            'Facebook' => '$552.39 billion USD',
+            'Hon Hai Precision' => '$38.72 billion USD',
+            'Tencent' => '$3.02 trillion USD',
+            'Oracle' => '$180.54 billion USD',
+        ];
+
+        return $this->render('attendance/index.html.twig', [
+            'data' => $data
+        ]);
+    }
+}
---
+++ src/Entity/Admin.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class Admin implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fullname;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+		public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+
+		public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+		public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
